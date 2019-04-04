c9aad7fcdef26917527e441ed560a442d07a54fa
NiveditaV96 <43562959+NiveditaV96@users.noreply.github.com>
2019-04-04 17:41:46 +0530

    employeeMgmtWebApi

---
+++ EmployeeMgmt/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using EmployeeMgmt.Models;
+using EmployeeMgmt.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeMgmt.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private IEmployeeRepository _employeeRepository;
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Create(Employee employee)
+        {
+            _employeeRepository.Add(employee);
+            return ResponseMessage(Request.CreateResponse(HttpStatusCode.Created));
+            
+        }
+
+        public IHttpActionResult GetAll()
+        {
+            return Ok(_employeeRepository.GetAll());
+        }
+
+        public IHttpActionResult GetById(int id)
+        {
+            var employee = _employeeRepository.GetById(id);
+            return Ok(employee);
+        }
+
+
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok(); 
+                //Ok(_employeeRepository.Delete(id));
+        }
+
+
+
+
+
+
+    }
+}
---
+++ EmployeeMgmt/Repository/IEmployeeRepository.cs
@@ -0,0 +1,21 @@
+﻿using EmployeeMgmt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeMgmt.Repository
+{
+    public interface IEmployeeRepository 
+    {
+        void Add(Employee emp);
+
+        List<Employee> GetAll();
+        Employee GetById(int id);
+
+        Employee Update(Employee employee);
+        void Delete(int id);
+
+    }
+}
